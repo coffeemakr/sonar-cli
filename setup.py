4cbb8df89fda924bc8d95784e5f03cfe079badcb
coffeemakr <git@unstable.ch>
2020-02-29 14:48:23 +0100

    Include requirements in the setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,23 @@
 
 from distutils.core import setup
 
+import pathlib
+
+import pkg_resources
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+
 setup(name='Sonar CLI',
       version='1.0',
       description='Command line interface to communicate with the project sonar open data.',
       packages=['sonar'],
       entry_points={
           'console_scripts': ['sonar=sonar:cli'],
-      })
+      },
+      install_requires=install_requires)
